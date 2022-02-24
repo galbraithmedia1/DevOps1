68ac978df5b564215c945adfbb5c541ec89b8aa1
galbraithmedia1 <59584919+galbraithmedia1@users.noreply.github.com>
2022-02-24 12:10:53 -0700

    push

--- server/index.js
+++ server/index.js
@@ -3,6 +3,17 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'a494eb9998984a8f800efe2ca992db0c',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
